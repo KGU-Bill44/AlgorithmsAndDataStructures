b4ac3d75beffcd448140847577373a5b38b08642
Bill44 <denyc9@mail.ru>
2025-02-08 14:31:09 +0300

    Laboratory Work One

---
+++ LaboratoryWorkOne/ConsoleUi.cs
@@ -0,0 +1,49 @@
+﻿namespace LaboratoryWorkOne;
+
+internal class ConsoleUi
+{
+    /// <summary>
+    /// Вход в консолное прилоджение.
+    /// </summary>
+    public static void Run()
+    {
+        try
+        {
+            PrintWelcome();
+            PrintInfoTask();
+
+            int number = GetNumberFromConsole("число");
+            
+            Console.WriteLine("Вывод: " + NumberUtil.IsPowerOfTwo(number));
+        }
+        catch (FormatException e)
+        {
+            Console.WriteLine("Введеная строка не соответсвует формату числа");
+        }
+        catch (OverflowException e)
+        {
+            Console.WriteLine("Число вышло за рамки допустимых значений");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ошибка: " + ex.Message);
+        }
+    }
+    
+    private static void PrintWelcome()
+    {
+        Console.WriteLine("Приветсвую!");
+    }
+    
+    private static void PrintInfoTask()
+    {
+        Console.Write("Программа проверяет степень двойки.\n");
+    }
+    
+    private static int GetNumberFromConsole(string nameForVarible)
+    {
+        Console.Write($"Введите {nameForVarible}: ");
+        string anyString = Console.ReadLine();
+        return int.Parse(anyString);
+    }
+}
---
+++ LaboratoryWorkOne/NumberUtil.cs
@@ -0,0 +1,26 @@
+﻿namespace LaboratoryWorkOne;
+
+public static class NumberUtil
+{
+    public static bool IsPowerOfTwo(int number)
+    {
+        if (number < 1)
+        {
+            throw new ArgumentException("Число не натуральнеое.");
+        }
+        
+        if (number == 1)
+        {
+            return true;
+        }
+
+        if (number % 2 == 0)
+        {
+            return IsPowerOfTwo(number / 2);
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
