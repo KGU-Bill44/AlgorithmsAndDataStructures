2da2ff398a1a4f7800c5688dcdfd9e0bceaaa97d
Bill44 <denyc9@mail.ru>
2025-02-24 09:20:09 +0300

    StartQueueLab

---
+++ LaboratoryWorkFive/QueueImpl.cs
@@ -0,0 +1,46 @@
+﻿namespace LaboratoryWorkFive;
+
+public class QueueImpl<T>
+{
+    private T[] items;
+    private int size;
+    private int tail;
+    private int head;
+
+    public QueueImpl()
+    {
+        
+    }
+
+    public QueueImpl(T[] items)
+    {
+        head = 0;
+        size = items.Length;
+        tail = size - 1;
+        this.items = items;
+    }
+
+    public void Push(T item)
+    {
+        tail = tail + 1;
+        
+        if (size - head <= tail)
+        {
+            size = size / 2 + size;
+            Array.Resize(ref items, size);
+        }
+    }
+
+    private void SetItem(T item, int tail)
+    {
+        items[tail] = item;
+    }
+
+    public T Take()
+    {
+        T item = items[head];
+        head = head - 1;
+        size = size - 1;
+        return item;
+    }
+}
