941b344cddab6a3b6565e2389038ce7f8032b5c5
Bill44-NotAlone <denyc9@mail.ru>
2025-02-24 13:00:44 +0300

    fix

--- LaboratoryWorkFive/QueueImpl.cs
+++ LaboratoryWorkFive/QueueImpl.cs
@@ -1,6 +1,8 @@
-﻿namespace LaboratoryWorkFive;
+﻿using System.Collections;
 
-public class QueueImpl<T>
+namespace LaboratoryWorkFive;
+
+public class QueueImpl<T> : IEnumerable<T>
 {
     private T[] items;
     private int size;
@@ -9,38 +11,118 @@ public class QueueImpl<T>
 
     public QueueImpl()
     {
-        
+        size = 0;
+        items = new T[10];
+        head = tail = 0;
     }
 
     public QueueImpl(T[] items)
     {
         head = 0;
         size = items.Length;
-        tail = size - 1;
+        tail = size;
         this.items = items;
     }
 
     public void Push(T item)
     {
-        tail = tail + 1;
-        
-        if (size - head <= tail)
+        if (IsArrayOverflowing())
         {
-            size = size / 2 + size;
-            Array.Resize(ref items, size);
+            Increase();
         }
+
+        items[tail] = item;
+        MoveNext(ref tail);
+        size = size + 1;
+        
     }
 
-    private void SetItem(T item, int tail)
+    private bool IsArrayOverflowing()
     {
-        items[tail] = item;
+        return size == items.Length;
+    }
+
+    private void Increase()
+    {
+        int newSize = items.Length + items.Length / 2;
+        T[] nextItems = new T[newSize];
+
+        if (head < tail)
+        {
+            Array.Copy(items, head, nextItems, 0, size);
+        }
+        else
+        {
+            Array.Copy(items, head, nextItems, 0, size - head);
+            Array.Copy(items, 0, nextItems, size - head, tail);
+        }
+
+        items = nextItems;
+        tail = size;
+        head = 0;
+    }
+
+    private void MoveNext(ref int index)
+    {
+        if (index < items.Length - 1)
+        {
+            index = index + 1;
+        }
+        else
+        {
+            index = 0;
+        }
     }
 
     public T Take()
     {
         T item = items[head];
-        head = head - 1;
+        items[head] = default;
+        MoveNext(ref head);
         size = size - 1;
+
         return item;
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return new QueueIEnumeratorImpl(items);
+    }
+    
+    private class QueueIEnumeratorImpl : IEnumerator<T>
+    {
+        private T[] array;
+        private int currentIndex;
+        
+        public QueueIEnumeratorImpl(T[] array)
+        {
+            this.array = array;
+            currentIndex = 0;
+        }
+
+
+        public bool MoveNext()
+        {
+            currentIndex = currentIndex + 1;
+            return currentIndex < array.Length;
+        }
+
+        public void Reset()
+        {
+            currentIndex = -1;
+        }
+
+        object? IEnumerator.Current => this.Current;
+
+        public void Dispose()
+        {
+        }
+
+        public T Current => array[currentIndex];
     }
 }
--- LaboratoryWorkFive/Starter.cs
+++ LaboratoryWorkFive/Starter.cs
@@ -1,3 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine(new Queue<int>());
+using LaboratoryWorkFive;
+
+QueueImpl<int> impl = new QueueImpl<int>();
+
+for (int i = 0; i < 10; i++)
+{
+    impl.Push(i);
+}
+
+foreach (var i in impl)
+{
+    Console.WriteLine(i);
+}
