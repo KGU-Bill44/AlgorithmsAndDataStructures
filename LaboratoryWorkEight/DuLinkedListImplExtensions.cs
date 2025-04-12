460769c5ae59a3b2f687f82b23f064363f95a4e9
Bill44 <denyc9@mail.ru>
2025-04-12 14:28:33 +0300

    fix

--- LaboratoryWorkEight/DuLinkedListImplExtensions.cs
+++ LaboratoryWorkEight/DuLinkedListImplExtensions.cs
@@ -4,18 +4,38 @@ public static class DuLinkedListImplExtensions
 {
     public static bool ContainsAll<T>(this DuLinkedListImpl<T> list, IEnumerable<T> enumerable)
     {
-        return enumerable.All(list.Contains);
+        List<T> anes = enumerable.ToList();
+        
+        if (!anes.Any())
+        {
+            return true;
+        }
+
+        int index = list.FindIndex(anes.First());
+
+        if (index == -1 || index + anes.Count > list.Count)
+        {
+            return false;
+        }
+
+        for (int i = 0; i < anes.Count(); i++)
+        {
+            if (!list[i + index].Equals(anes[i]))
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public static void Sort<T>(this DuLinkedListImpl<T> list) where T : IComparable
     {
         int index = 0;
-        
 
         while (index < list.Count)
         {
             T element = list[index];
-            
             int lestIndex = 0;
             while (lestIndex < index)
             {
@@ -24,6 +44,7 @@ public static class DuLinkedListImplExtensions
                     lestIndex = lestIndex + 1;
                     continue;
                 }
+
                 list.InsertBefore(element, lestIndex);
                 list.RemoveAfter(index);
                 break;
